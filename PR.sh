bbbb6f17b261e91aca6dae6c929a0c0e3b981391
ujjwalk <ujjwalkeshri@hotmail.com>
2025-10-28 00:59:09 +0530

    3rd PR_demo

--- PR.sh
+++ PR.sh
@@ -3,6 +3,7 @@
 <<info
 Daily Backup of shellscript
 This line is added to Daily Backup Script for confirmation that it isworking fine with CRON
+Remember to close CRON if its not required
 eg.
 ./backup.sh <source> <dest>
 
